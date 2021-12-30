fc64011f6431c15ef5508424b8a7dc94e6da8ed1
Jatin0097 <jatin.kaushik3409@gmail.com>
2021-12-31 00:31:07 +0530

    summary

---
+++ summary.py
@@ -0,0 +1,23 @@
+# ********summary of chapter one **********
+#print function 
+print ("hello'hello'")   #ONLY USE OPPOSITE COTS INSIDE {BAHAR SINGLE TO ANDAR DOUBLE COTS @VICE VRSA}
+#OR USE {\"="} OR{\'='}
+
+# escape sequences
+# print ("line A\t line B")      #{\n used for nect line }
+#  {\t used for add space }
+print("this is backslash\\\\")
+
+#escape sequence as anormal text
+# print ("line A \\n lineB")
+
+#python as a calculator 
+# print (2+3)
+# print(2-3)
+print(4/2) #float division       {gives uh floating point like 2.0}
+print (4//2)#integer division    {give uh accurate division without any flaot or point like 2}
+print(3/2) 
+print (2**3**2)
+#variables
+#naming rule for variable 
+#convention for variable naming 
