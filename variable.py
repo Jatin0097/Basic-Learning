bb41e4b0fe610bf393a83b1a59a3926a4a975d91
Jatin0097 <jatin.kaushik3409@gmail.com>
2021-12-30 12:05:11 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,15 @@
+
+name ="jatin"
+print(name)
+name="shilpi"
+print(name)
+name=123
+print(name)
+#Rule.1 =variable cannot start from any no.like 
+#1number =5
+#variable start with any lette ,_ for first letter  
+#_name = "jatin"
+#u1sd=34
+#naming convention for variable:
+# user_one_name = "surender" #this type of writing known as snake case writing .{used mostly in python}
+userOneName = "mohit" #camel case writing {used in java}
